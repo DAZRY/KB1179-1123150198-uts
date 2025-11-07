28cd9499110de9d858959341787c57459953694f
Lendra <lendradzr77@gmail.com>
2025-11-07 20:18:01 +0700

    membuat material 3

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Nim : 1123150198, Syailendra Fas Faye",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
