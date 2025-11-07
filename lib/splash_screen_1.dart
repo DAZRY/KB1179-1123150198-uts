2c231288baac9c759e83e750421f8fd99d46de6c
Lendra <lendradzr77@gmail.com>
2025-11-07 20:31:39 +0700

    membuat image dalam column

--- lib/splash_screen_1.dart
+++ lib/splash_screen_1.dart
@@ -5,6 +5,27 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.green,
+                image: DecorationImage(
+                  image: AssetImage("/images/Roblox1.png"),
+                  fit: BoxFit.cover,) 
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
